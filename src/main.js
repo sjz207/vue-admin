6a3472c15aa0c5dc5044f1ba6d3edebd9d2e183a
lukw510903926 <13507615840@163.com>
2018-08-02 21:45:01 +0800

    element ui 全局大小配置

--- src/main.js
+++ src/main.js
@@ -2,21 +2,21 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import ElementUI from 'element-ui'
-import Bus from '@/util/Bus'
-import 'element-ui/lib/theme-chalk/index.css';
+import 'element-ui/lib/theme-chalk/index.css'
 import '@/assets/css/common.css'
-import storage from '@/util/store'
-import http from '@/util/AxiosHttp'
 import ZkTable from 'vue-table-with-tree-grid'
 import cookies from 'vue-cookies'
+import Bus from '@/util/Bus'
+import storage from '@/util/store'
+import http from '@/util/AxiosHttp'
 
-Vue.use(cookies)
-Vue.use(ZkTable)
+Vue.use(cookies);
+Vue.use(ZkTable);
 Vue.use(ElementUI, {size: 'small', zIndex: 3000});
-Vue.prototype.$storage = storage
-Vue.prototype.$http = http
-Vue.config.productionTip = false
-const bus = Bus
+Vue.prototype.$storage = storage;
+Vue.prototype.$http = http;
+Vue.config.productionTip = false;
+const bus = Bus;
 
 new Vue({
   el: '#app',
@@ -27,4 +27,4 @@ new Vue({
     storage
   },
   template: '<App/>'
-})
+});
